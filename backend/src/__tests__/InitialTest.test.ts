d18559a66bc025dab857681c9c993322a2ba101d
hirokita117 <r4.henke@gmail.com>
2021-07-04 21:41:03 +0900

    back: add InitialTest

---
+++ backend/src/__tests__/InitialTest.test.ts
@@ -0,0 +1,7 @@
+import { AlgoGameUrl } from '../config/app';
+
+describe('InitialTest', () => {
+  test('AlgoGameUrl is not undefined', () => {
+    expect(AlgoGameUrl).not.toBeUndefined();
+  });
+});
