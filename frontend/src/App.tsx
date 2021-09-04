6e62ed50f9598cd726d4dd67695c2c6aea933c6a
hirokita117 <r4.henke@gmail.com>
2021-09-04 17:23:30 +0900

    react-router-domを入れてみた

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,20 @@
 import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 
 import { AlgoGameApi } from './config/app';
 import logo from './logo.svg';
 
 import './App.css';
 
-export const App: React.VFC = () => {
-  const [msg, setMsg] = useState('');
-  useEffect(() => {
-    fetch(AlgoGameApi)
-      .then((res) => res.json())
-      .then((data) => setMsg(data.message));
-  }, [msg]);
+const About: React.VFC = () => {
+  return <div>About</div>;
+};
 
+const Topics: React.VFC = () => {
+  return <div>Topics</div>;
+};
+
+const Home = (props: { msg: string }) => {
   return (
     <div className="App">
       <header className="App-header">
@@ -29,8 +31,49 @@ export const App: React.VFC = () => {
         >
           Learn React
         </a>
-        <p>{msg}</p>
+        <p>{props.msg}</p>
       </header>
     </div>
+  );
+};
+
+export const App: React.VFC = () => {
+  const [msg, setMsg] = useState('');
+  useEffect(() => {
+    fetch(AlgoGameApi)
+      .then((res) => res.json())
+      .then((data) => setMsg(data.message));
+  }, [msg]);
+
+  return (
+    <>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/topics">Topics</Link>
+            </li>
+          </ul>
+
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/topics">
+              <Topics />
+            </Route>
+            <Route path="/">
+              <Home msg={msg} />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </>
   );
 };
